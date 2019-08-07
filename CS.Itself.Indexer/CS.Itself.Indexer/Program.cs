67a42c48355404c21c33670bcc530b9fb64aa369
Anton Miroshkin <anton.miroshkin@gmail.com>
2019-08-07 17:06:19 +0300

    Init commit

---
+++ CS.Itself.Indexer/CS.Itself.Indexer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CS.Itself.Indexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
